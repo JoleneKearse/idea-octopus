6c4e3048f45517d700c0ed09ef6ca6d68e367ce9
Jolene Kearse <pharaohnutz@gmail.com>
2022-08-23 10:32:18 -0400

    removed console.log

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const text = document.getElementById("text");
 fetch("https://apis.scrimba.com/bored/api/activity")
     .then((response) => response.json())
     .then((data) => {
-        console.log(data);
         ideaBtn.addEventListener("click", function () {
             text.textContent = data.activity;
         });
